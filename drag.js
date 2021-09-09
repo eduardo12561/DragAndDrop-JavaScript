56fe9d66278e0ac23531816d0d636d68cd2caa27
Eduardo Breda <edu12561@hotmail.com>
2021-09-09 12:51:53 -0300

    create a responsive drag and drop list

---
+++ drag.js
@@ -0,0 +1,117 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const frameWorks = [
+    'Ruby on Rails',
+    'Laravel',
+    'Django',
+    'ASP.NET',
+    'Express',
+    'Spring',
+    'Angular',
+    'Ember',
+    'VueJS',
+    'React'
+];
+
+// Store listitems
+const listItems = [];
+
+let dragStartIndex;
+
+createList()
+
+
+// Insert List items into DOM
+function createList() {
+    [...frameWorks]
+    .map(a => ({value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(a => a.value)
+    .forEach((frameW, index) => {
+        const listItem = document.createElement('li');
+
+        listItem.setAttribute('data-index', index);
+
+        listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="frame-name">${frameW}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+        `;
+
+      listItems.push(listItem);
+
+      draggable_list.appendChild(listItem);
+    });
+
+    addEventListener();
+}
+
+function dragStart() {
+    dragStartIndex = +this.closest('li').getAttribute('data-index'); 
+    console.log(this.closest('li'))
+}
+  
+function dragEnter() {
+    this.classList.add('over');
+}
+
+function dragLeave() {
+    this.classList.remove('over');
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragDrop() {
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+}
+
+
+// Check the order of list items
+function checkOrder() {
+    listItems.forEach((listItem, index) => {
+        const frameName = listItem.querySelector('.draggable').innerText.trim();
+
+        if(frameName !== frameWorks[index]) {
+            listItem.classList.add('wrong');
+        } else {
+            listItem.classList.remove('wrong')
+            listItem.classList.add('right')
+        }
+    });
+}
+
+function addEventListener() {
+    const draggables = document.querySelectorAll('.draggable');
+    const dragListItems = document.querySelectorAll('.draggable-list li');
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart);
+      });
+
+    dragListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver);
+        item.addEventListener('drop', dragDrop);
+        item.addEventListener('dragenter', dragEnter);
+        item.addEventListener('dragleave', dragLeave);
+    });
+
+    
+}
+
+check.addEventListener('click', checkOrder);
